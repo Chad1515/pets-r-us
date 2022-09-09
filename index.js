c0694e37efd7411f08fc2a046060fc47fb46d70e
Chad ONeal <chadoneal3@gmail.com>
2022-09-09 17:31:12 -0400

    pets-r-us

--- index.js
+++ index.js
@@ -2,54 +2,61 @@
 ==============================================
 ; Title: Assignment 4.2 pets-r-us
 ; Author: Chad ONeal
-; Date: 08/31/2022
+; Date: 09/09/2022
+; Resource: Resource: https://www.youtube.com/watch?v=lYVKbAn5Od0
 ; Description: index.js for pets-r-us
 ==============================================
 */
 
-// Require statements
+//requiring express & ejs
 const express = require('express');
-const path = require('path');
-const app = express();
 const ejs = require('ejs');
+const app = express();
+const path = require('path');
  
-// Renders HTML with EJS
+//render HTML w/ EJS
 app.engine('html', require('ejs').__express);
 
-// Sets the views folder as the path for HTML files
+//set views folder as path for HTML files
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
 app.set('views', './views');
+app.set('view engine', 'html');
  
-// Sets the public folder as the path for static files
+//set public folder as path for static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// App listens on this port
+//app listen on port 3000
 const PORT = process.env.PORT || 3000;
 
-// links the corresponding HTML files to the correct path and sets their title
+//sends landing/home page
 app.get('/', (req, res) => {
     res.render('index', {
-        title: 'Pets-R-Us Home',
-    })
-});
-app.get('/grooming', (req, res) => {
-    res.render('grooming', {
-        title: 'Pets-R-Us Dog Grooming',
-    })
-});
-app.get('/boarding', (req, res) => {
-    res.render('boarding', {
-        title: 'Pets-R-Us Dog Boarding',
-    })
-});
-app.get('/training', (req, res) => {
-    res.render('training', {
-        title: 'Pets-R-Us Dog Training',
+        title: 'Home',
     })
 });
 
-// Tells the user that the application has started on the declared port
+//sends grooming page
+app.get('/grooming', (req, res) => {
+    res.render('grooming', {
+        title: 'Grooming',
+    })
+});
+
+//sends boarding page
+app.get('/boarding', (req, res) => {
+    res.render('boarding', {
+        title: 'Boarding',
+    })
+});
+
+//sends training page
+app.get('/training', (req, res) => {
+    res.render('training', {
+        title: 'Training',
+    })
+});
+
+//port declared & message to console that the app has started
 app.listen(PORT, () => {
-    console.log('Application started and listening on port ' + PORT);
+    console.log('The App has started and listens on port ' + PORT);
 });
