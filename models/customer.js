00fe55eac611a019fbafb30173be3cd4ae5f0eb4
Chad ONeal <cdoneal@my365.bellevue.edu>
2022-09-22 12:11:39 -0400

    pets-r-us

--- models/customer.js
+++ models/customer.js
@@ -1,11 +1,24 @@
+/**
+==============================================
+; Title: Assignment 4.2 pets-r-us
+; Author: Professor Krasso
+; Modified by: Chad ONeal
+; Date: 09/22/2022
+; Description: index.js for pets-r-us
+==============================================
+*/
+
+// requiring mongoose to connect to MongoDB
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// creation of data to be held in mongo from registration form
 let customerSchema = new Schema({
     email: { type: String, required: true, unique: true },
     customerID: {type: String, required: true, unique:true},
 })
 
+//exports data from app into the mongoDB database
 module.exports = mongoose.model('Customer', customerSchema);
 
 
